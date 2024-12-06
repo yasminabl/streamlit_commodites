457ba72db87616fb8d3567c37481ac5678a04cf4
yass <154814897+Pilot2230@users.noreply.github.com>
2024-12-06 11:03:51 +0100

    Update Accueil.py

--- PROJET_FI/Accueil.py
+++ PROJET_FI/Accueil.py
@@ -16,7 +16,12 @@ st.set_page_config(
     layout="wide",  #pour maximiser l'espace
 )
 
+import sys
+import streamlit as st
 
+st.write("Python executable:", sys.executable)
+st.write("Python version:", sys.version)
+st.write("Conda environment:", sys.prefix)
 
 
 def create_df(name, url, key):
